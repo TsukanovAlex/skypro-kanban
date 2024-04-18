f40dacb801e59e8e8cb46af734a9f64ec80067d1
*Aleksey <*atsukanov89@gmail.com*>
2024-04-18 19:40:41 +0300

    Перенес стили PopExit

--- React-project/src/components/popups/PopExit/PopExit.jsx
+++ React-project/src/components/popups/PopExit/PopExit.jsx
@@ -1,24 +1,29 @@
+import { Link } from "react-router-dom"
+import * as S from "../PopExit/popExit.styled"
+
 
 function PopExit(){
-    return (<div className="pop-exit" id="popExit">
-    <div className="pop-exit__container">
-      <div className="pop-exit__block">
-        <div className="pop-exit__ttl">
+    return (
+    <S.PopExit id="popExit">
+    <S.PopExitContainer>
+      <S.popExitBlock>
+        <S.PopExitTtl>
           <h2>Выйти из аккаунта?</h2>
-        </div>
-        <form className="pop-exit__form" id="formExit" action="#">
-          <div className="pop-exit__form-group">
-            <button className="pop-exit__exit-yes _hover01" id="exitYes">
-              <a href="modal/signin.html">Да, выйти</a>{" "}
-            </button>
-            <button className="pop-exit__exit-no _hover03" id="exitNo">
-              <a href="main.html">Нет, остаться</a>{" "}
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>)
+        </S.PopExitTtl>
+        
+      
+          <S.popExitForm>
+            <S.ButtonExitYes>
+              <Link to="/login">Да, выйти</Link>
+            </S.ButtonExitYes>
+            <S.ButtonExitNo>
+              <Link to="/">Нет, остаться</Link>{" "}
+            </S.ButtonExitNo>
+          </S.popExitForm>
+  
+      </S.popExitBlock>
+    </S.PopExitContainer>
+  </S.PopExit>)
 
 }
 export default PopExit
